3bdc67280391401f34137964c7b563bcdcaa4a4a
JOka <110078929+DevSpeed-JOka@users.noreply.github.com>
2022-08-07 06:08:02 +0100

    Add files via upload

---
+++ Hub.lua
@@ -0,0 +1,22 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Speed's Hub", "BloodTheme")
+
+-- Main
+
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
+local BloxFruits = Window:NewTab("BloxFruits")
+local BloxFruitsSection = BloxFruits:NewSection("BloxFruits")
+
+MainSection:NewButton("TriggerBot", "Most Games", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/VapingCat/Open-Source-TriggerBot/main/script.lua'))()
+end)
+
+MainSection:NewButton("Esp", "Most Games", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/fatesc/fates-esp/main/main.lua'))()
+end)
+
+BloxFruitsSection:NewButton("BloxFruits", "Most Games", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+end)
+
